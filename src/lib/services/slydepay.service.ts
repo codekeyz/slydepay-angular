b9aacc4f5b66d4079c49519bb18266b8033f3605
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-15 13:32:37 +0000

    Added functions to slydepayservice

--- src/lib/services/slydepay.service.ts
+++ src/lib/services/slydepay.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SlydepayConfig } from '../modules/slydepay.module';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,34 @@ export class SlydepayService {
     @Inject('config') private config: SlydepayConfig,
     private http: HttpClient
   ) {}
+  private baseUrl = 'https://app.slydepay.com.gh/api/merchant';
+
+  createAn;
+
+  getlistOptions(): Observable<any> {
+    return this.http.post(`${this.baseUrl}/invoice/payoptions`, this.config);
+  }
+
+  createInvoice(options, sendInvoice: boolean = false): Observable<any> {
+    return this.http.post(
+      `${this.baseUrl}/invoice/create`,
+      (options.sendInvoice = sendInvoice)
+    );
+  }
+
+  sendInvoice(options): Observable<any> {
+    return this.http.post(`${this.baseUrl}/invoice/send`, options);
+  }
+
+  checkPaymentStatus(options): Observable<any> {
+    return this.http.post(`${this.baseUrl}/invoice/checkstatus`, options);
+  }
+
+  confirmTransaction(options): Observable<any> {
+    return this.http.post(`${this.baseUrl}/transaction/confirm`, options);
+  }
+
+  cancelTransaction(options): Observable<any> {
+    return this.http.post(`${this.baseUrl}/transaction/confirm`, options);
+  }
 }
