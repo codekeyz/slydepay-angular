2cb87d667643e1e9c8358e212135d71b1d509cd4
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-15 14:58:39 +0000

    Added slydepay functions to slydepay service

--- src/lib/services/slydepay.service.ts
+++ src/lib/services/slydepay.service.ts
@@ -2,6 +2,15 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SlydepayConfig } from '../modules/slydepay.module';
 import { Observable } from 'rxjs';
+import {
+  SlydepayResponse,
+  ListPayOptionsResult,
+  CreateInvoiceResult,
+  Invoice,
+  SendInvoice,
+  CheckPaymentStatus,
+  Transaction
+} from '../helpers/slydepay.models';
 
 @Injectable({
   providedIn: 'root'
@@ -13,30 +22,59 @@ export class SlydepayService {
   ) {}
   private baseUrl = 'https://app.slydepay.com.gh/api/merchant';
 
-  getlistOptions(): Observable<any> {
-    return this.http.post(`${this.baseUrl}/invoice/payoptions`, this.config);
-  }
-
-  createInvoice(options, sendInvoice: boolean = false): Observable<any> {
-    return this.http.post(
-      `${this.baseUrl}/invoice/create`,
-      (options.sendInvoice = sendInvoice)
+  getlistOptions(): Observable<SlydepayResponse<ListPayOptionsResult>> {
+    return this.http.post<SlydepayResponse<ListPayOptionsResult>>(
+      `${this.baseUrl}/invoice/payoptions`,
+      this.config
     );
   }
 
-  sendInvoice(options): Observable<any> {
+  createInvoice(
+    invoice: Invoice
+  ): Observable<SlydepayResponse<CreateInvoiceResult>> {
+    return this.http.post<SlydepayResponse<CreateInvoiceResult>>(
+      `${this.baseUrl}/invoice/create`,
+      invoice
+    );
+  }
+
+  createInvoiceAndSend(
+    invoice: SendInvoice
+  ): Observable<SlydepayResponse<CreateInvoiceResult>> {
+    return this.http.post<SlydepayResponse<CreateInvoiceResult>>(
+      `${this.baseUrl}/invoice/create`,
+      invoice
+    );
+  }
+
+  sendInvoice(options: SendInvoice): Observable<any> {
     return this.http.post(`${this.baseUrl}/invoice/send`, options);
   }
 
-  checkPaymentStatus(options): Observable<any> {
-    return this.http.post(`${this.baseUrl}/invoice/checkstatus`, options);
+  checkPaymentStatus(
+    options: CheckPaymentStatus
+  ): Observable<SlydepayResponse<string>> {
+    return this.http.post<SlydepayResponse<string>>(
+      `${this.baseUrl}/invoice/checkstatus`,
+      options
+    );
   }
 
-  confirmTransaction(options): Observable<any> {
-    return this.http.post(`${this.baseUrl}/transaction/confirm`, options);
+  confirmTransaction(
+    options: Transaction
+  ): Observable<SlydepayResponse<string>> {
+    return this.http.post<SlydepayResponse<string>>(
+      `${this.baseUrl}/transaction/confirm`,
+      options
+    );
   }
 
-  cancelTransaction(options): Observable<any> {
-    return this.http.post(`${this.baseUrl}/transaction/confirm`, options);
+  cancelTransaction(
+    options: Transaction
+  ): Observable<SlydepayResponse<string>> {
+    return this.http.post<SlydepayResponse<string>>(
+      `${this.baseUrl}/transaction/confirm`,
+      options
+    );
   }
 }
